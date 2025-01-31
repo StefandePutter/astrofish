8017535361a0a1a404a9747b9766d4e5c436ad7b
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-01-31 13:55:36 +0100

    added watershot start

--- Assets/Scrips/FishShooter.cs
+++ Assets/Scrips/FishShooter.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class FishShooter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Fish Fish;
+    [SerializeField] private GameObject WaterShotPrefab;
+    [SerializeField] private float fireRate = 1f;
+    [SerializeField] private float spawnDistance = 1000f;
+    // [SerializeField] private Transform firepoint;
+    private float nextFireTime = 0f;
+    GameObject waterShot;
+
     void Start()
+    {
+        Fish = gameObject.GetComponent<Fish>(); 
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Shoot() // shoot projectile and delay function
+    { 
+        if (Time.time >= nextFireTime)
+        {
+            //shoot function
+
+            GameObject tempObj = new GameObject("Temp");
+            Transform temp = tempObj.transform; // Get its transform
+            temp.SetParent(transform);
+            temp.position = transform.position;
+            Vector3 mousePosition = Fish.mousePos;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector3 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+            temp.up = direction;
+
+            waterShot = Instantiate(WaterShotPrefab, temp.position, temp.rotation);
+            
+            Destroy(tempObj);
+
+
+            nextFireTime = Time.time + 1f / fireRate;
+        }
         
+        Debug.Log("Attack towards: " + Fish.mousePos);
+
+
+    }
+
+    public void OnAttack() //set bool to false so shooting can be used
+    {
+        if (Fish.isBubbled == false)
+        {
+            Shoot();
+        }
+        else
+        {
+            Fish.isBubbled = false;
+            Shoot();
+        }
+        Debug.Log(Time.time);
+
     }
 }
